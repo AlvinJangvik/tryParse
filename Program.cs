ea8135d1d1e8bc8a4342190e7d138550c005fb10
AlvinJangvik <54435532+AlvinJangvik@users.noreply.github.com>
2020-11-17 10:37:04 +0100

    Uppgiften

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryParseReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultat;
+            if(!int.TryParse(Console.ReadLine(), out resultat))
+            {
+                Console.WriteLine("Skriv ett heltal");
+            }
+        }
+
+        public static bool TryParse(string s, out int result)
+        {
+            try
+            {
+                result = int.Parse(s);
+                return true;
+            }
+            catch
+            {
+                result = 0;
+                return false;
+            }
+        }
+    }
+}
